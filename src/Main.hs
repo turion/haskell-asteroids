b80a7f4d3652274b616141ff175e778c469de7b1
Christian Steinmeyer <christian.steinmeyer.1993@gmail.com>
2016-05-09 15:50:59 +0200

    added simple example from https://wiki.haskell.org/Yampa/reactimate#Example

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,21 @@
-main = putStrLn "Hello World"
+import Control.Monad
+import Data.IORef
+import Data.Time.Clock
+import FRP.Yampa
+ 
+twoSecondsPassed :: SF () Bool
+twoSecondsPassed = time >>> arr (> 2)
+
+main :: IO ()
+main = do
+  t <- getCurrentTime
+  timeRef <- newIORef t
+  let init        = putStrLn "Hello... wait for it..."
+      actuate _ x = when x (putStrLn "World!") >> return x
+      sense   _   = do
+        now      <- getCurrentTime
+        lastTime <- readIORef timeRef
+        writeIORef timeRef now
+        let dt = now `diffUTCTime` lastTime
+        return (realToFrac dt, Just ())
+  reactimate init sense actuate $ twoSecondsPassed
