32fe71252af26ed95179f4f69435714ffc9e6521
Christian Steinmeyer <christian.steinmeyer.1993@gmail.com>
2016-05-15 11:56:40 +0200

    added bouncing ball code to main, compile errors

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,28 @@
 import Control.Monad
+import Control.Arrow
 import Data.IORef
 import Data.Time.Clock
 import FRP.Yampa
+
+type Pos = Double
+type Vel = Double
+
+fallingBall :: Pos -> Vel -> SF()(Pos, Vel)
+fallingBall y0 v0 = proc () -> do
+    v <- (v0+)^<< integral -< -9.81 -- gravity
+    y <- (y0+)^<< integral -< v
+    returnA -< (y,v)
+
+fallingBall' :: Pos -> Vel -> SF()((Pos, Vel), Event(Pos, Vel))
+fallingBall' y0 v0 = proc () -> do
+    yv@(y, _) <- fallingBall y0 v0 -< ()
+    hit       <- edge              -< y <= 0
+    returnA -< (yv, hit 'tag' yv)
+
+bouncingBall :: Pos -> SF()(Pos, Vel)
+bouncingBall y0 = bbAux y0 0.0
+    where bbAux y0 v0 =
+        switch(fallingBall' y0 v0) $ \(y, v) -> bbAux y (-v)
  
 twoSecondsPassed :: SF () Bool
 twoSecondsPassed = time >>> arr (> 2)
