0c6c751f4db979d848cadf2f03726858780ad0d9
Christian Steinmeyer <christian.steinmeyer.1993@gmail.com>
2016-06-08 07:19:28 +0200

    altered Datatypes structure, now easier

--- src/Datatypes.hs
+++ src/Datatypes.hs
@@ -1,5 +1,7 @@
 module Datatypes (
-  Ship
+  Vector(..),
+  GameObjectType,
+  GameObject(..)
   ) where
 
 data Vector = Vector {
@@ -8,46 +10,12 @@ data Vector = Vector {
   } deriving (Eq, Show)
 
 
-class Located a where
-  getLocation :: a -> Vector
+data GameObjectType = Ship | Asteroid 
+  deriving Show
 
-class Oriented a where
-  getOrientation :: a -> Vector
-
-class (Located a, Oriented a) => Displayable a where 
-  draw :: a -> IO()
-
--- SHIP --
-
-data Ship = Ship {
-  sLocation :: Vector,
-  sVelocity :: Vector,
-  sOrientation :: Vector
-  } deriving (Eq, Show)
-
-instance Located Ship where
-  getLocation = sLocation
-
-instance Oriented Ship where
-  getOrientation = sOrientation
-
-instance Displayable Ship where
-  draw ship = return ()
-
-
--- ASTEROID --
-
-data Asteroid = Asteroid {
-  aLocation :: Vector,
-  aVelocity :: Vector,
-  aOrientation :: Vector
-}
-
-instance Located Asteroid where
-  getLocation = aLocation
-
-instance Oriented Asteroid where
-  getOrientation = aOrientation
-
-instance Displayable Asteroid where
-  draw asteroid = return ()
+data GameObject = GameObject {
+  location :: Vector,
+  orientation :: Vector,
+  velocity :: Vector,
+  gameObjectType :: GameObjectType
+  }
